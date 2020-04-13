e4fba4e043f23c758b6629021e26fa63b7be1491
kykisk <kykisk@gmail.com>
2020-04-13 17:50:36 +0900

    Test2

--- Test1/src/com/common/CommonUtil.java
+++ Test1/src/com/common/CommonUtil.java
@@ -1,15 +1,17 @@
 package com.common;
 
-/**
- * @wi.implements MyProjecy/MP-510:001 Test
- * 
- * @author polar
- *
- */
+
 public class CommonUtil {
 	private String test1 = "";
 	
 	public static void test1() {
 		return;
+	}
+	
+	/**
+	 * @wi.implements MyProjecy/MP-510
+	 */
+	public String getAuthor() {
+		return "";
 	}
 }
